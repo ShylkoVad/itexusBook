101becb9e0a0f53a5bbbeb72c480acb9fe3989f5
ShylkoVad <shylkovad@gmail.com>
2024-08-23 12:12:20 +0300

    добавил класс Book и файл books.csv

---
+++ src/main/java/com/itexus/domain/Book.java
@@ -0,0 +1,15 @@
+package com.itexus.domain;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String description;
+}
