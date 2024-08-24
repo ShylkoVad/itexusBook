e349f6fc6a3beb680d7268824d873d32546ac34e
Vadzim <shylkovad@gmail.com>
2024-08-24 20:21:33 +0300

    обновление build.gradle.kts с пояснениями

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.26")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("junit:junit:4.13.2")
-    implementation("org.springframework:spring-context:5.3.10")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:4.13.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+    implementation("org.projectlombok:lombok:1.18.26") // добавление зависимостей lombok
+    testImplementation(platform("org.junit:junit-bom:5.9.1")) // управление версиями зависимостей, связанных с JUnit, с помощью BOM, что позволяет определить, какие версии различных библиотек следует использовать, чтобы они были совместимыми друг с другом
+    testImplementation("org.junit.jupiter:junit-jupiter") // добавление зависимости для написания и запуска тестов в Java
+    testImplementation("junit:junit:4.13.2") // добавления библиотеки JUnit, для написания и выполнения автоматизированных тестов на Java
+    implementation("org.springframework:spring-context:6.1.12") // добавление зависимости на библиотеку Spring Context,
+    implementation("org.springframework:spring-core:6.1.12") // добавление зависимости на библиотеку Spring Core
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2") // основной модуль для сериализации и десериализации JSON
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2") // содержит аннотации, такие как @JsonProperty, которые можно использовать в коде для управления сериализацией и десериализацией
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.2") // базовый модуль, который необходим для работы Jackson
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.17.2") // ля добавления зависимости на библиотеку Jackson Dataformat для работы с CSV файлами
+
 }
 
 tasks.test {
