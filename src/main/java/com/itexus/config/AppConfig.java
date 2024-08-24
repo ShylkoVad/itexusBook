3344c5c14cae1e1f6f99ce77d379b25dc97a23dc
Vadzim <shylkovad@gmail.com>
2024-08-24 17:18:20 +0300

    создал класс AppConfig.java

---
+++ src/main/java/com/itexus/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.itexus.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.itexus")
+public class AppConfig {
+}
