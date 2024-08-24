ea8397098f2d79aa3f2f0618076f40fa079adea7
Vadzim <shylkovad@gmail.com>
2024-08-24 17:17:51 +0300

    добавил аннотации для Book.java

--- src/main/java/com/itexus/domain/Book.java
+++ src/main/java/com/itexus/domain/Book.java
@@ -1,15 +1,16 @@
 package com.itexus.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Book {
-
+    @JsonProperty("id")
     private Long id;
-
+    @JsonProperty("title")
     private String title;
-
+    @JsonProperty("author")
     private String author;
-
+    @JsonProperty("description")
     private String description;
 }
