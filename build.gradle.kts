513bb39907650994277d3ff9e296bbd18de7a949
Vadzim <shylkovad@gmail.com>
2024-08-24 17:16:30 +0300

    добавил зависимости для gradle.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     implementation("org.projectlombok:lombok:1.18.26")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("junit:junit:4.13.2")
+    implementation("org.springframework:spring-context:5.3.10")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:4.13.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
 }
 
 tasks.test {
