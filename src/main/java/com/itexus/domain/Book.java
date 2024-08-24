8a28d65c2f8111e31d65527ec5b55f7aab200851
Vadzim <shylkovad@gmail.com>
2024-08-24 22:16:43 +0300

    обновление Book.java добавил аннотации для lombok

--- src/main/java/com/itexus/domain/Book.java
+++ src/main/java/com/itexus/domain/Book.java
@@ -1,13 +1,17 @@
 package com.itexus.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     @JsonProperty("id")
     private Long id;
