77d6a611a62c96d1c0a6ce26adebd253c621a841
ShylkoVad <shylkovad@gmail.com>
2024-09-04 13:49:07 +0300

    редактирование класса AppConfig - добавил метод messageSource

--- src/main/java/com/itexus/config/AppConfig.java
+++ src/main/java/com/itexus/config/AppConfig.java
@@ -1,9 +1,22 @@
 package com.itexus.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.itexus")
 public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        // Базовое имя файлов свойств (без расширения)
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8"); // Кодировка для чтения файлов
+        return messageSource;
+    }
 }
