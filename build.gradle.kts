bf5de8e116b7a03871838b8f272ea4d1586cb99a
Vadzim <shylkovad@gmail.com>
2024-09-26 22:08:06 +0300

    добавлены зависимости в build.gradle для логирования

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,12 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2") // основной модуль для сериализации и десериализации JSON
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2") // содержит аннотации, такие как @JsonProperty, которые можно использовать в коде для управления сериализацией и десериализацией
     implementation("com.fasterxml.jackson.core:jackson-core:2.17.2") // базовый модуль, который необходим для работы Jackson
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.17.2") // ля добавления зависимости на библиотеку Jackson Dataformat для работы с CSV файлами
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.17.2") // для добавления зависимости на библиотеку Jackson Dataformat для работы с CSV файлами
+    implementation("org.springframework:spring-aop:6.1.13") // зависимость для AOP
+    implementation("org.slf4j:slf4j-api:2.0.16") // зависимость для логирования с использованием SLF4J
+    implementation("ch.qos.logback:logback-classic:1.5.8") // реализация для SLF4J
+    implementation("org.aspectj:aspectjweaver:1.9.22") // зависимость AspectJ
+    implementation("org.aspectj:aspectjrt:1.9.22.1") // зависимость на AspectJ Runtime
 
 }
 
