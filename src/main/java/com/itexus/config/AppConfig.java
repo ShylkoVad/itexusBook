a7b8dc095b91e0dd29391985e2d5095853b8ef44
Vadzim <shylkovad@gmail.com>
2024-09-26 22:20:50 +0300

    внесена @EnableAspectJAutoProxy в классе AppConfig

--- src/main/java/com/itexus/config/AppConfig.java
+++ src/main/java/com/itexus/config/AppConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.itexus")
+@EnableAspectJAutoProxy
 public class AppConfig {
     @Bean
     public MessageSource messageSource() {
