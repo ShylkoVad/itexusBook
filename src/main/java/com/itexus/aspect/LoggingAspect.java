33fae825a6e6c0edc9c2f3d4fd84afd55e2bf360
ShylkoVad <shylkovad@gmail.com>
2024-09-27 10:47:55 +0300

    изменен класс LoggingAspect для корректного отображения вывода сообщений по языкам

--- src/main/java/com/itexus/aspect/LoggingAspect.java
+++ src/main/java/com/itexus/aspect/LoggingAspect.java
@@ -1,20 +1,32 @@
 package com.itexus.aspect;
 
+import com.itexus.controller.BookController;
+import com.itexus.util.ApplicationContext;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Aspect
 @Component
 public class LoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+    private  MessageSource messageSource;
+
+    public LoggingAspect(BookController controller) {
+        this.messageSource = controller.getMessageSource();
+    }
+
     @After("execution(* com.itexus.service..*(..))")
     public void logAfter(JoinPoint joinPoint) {
-        logger.info("Вызван метод: " + joinPoint.getSignature().toShortString());
-
+        Locale currentLocale = ApplicationContext.getInstance().getLocale();
+        logger.info(messageSource.getMessage("message.methodCalled", null, currentLocale)
+                + joinPoint.getSignature().toShortString());
     }
 }
