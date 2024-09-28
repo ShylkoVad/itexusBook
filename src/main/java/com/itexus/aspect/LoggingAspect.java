438be49111ede8fe4d3642da94048eb5fb12d559
Vadzim <shylkovad@gmail.com>
2024-09-28 10:57:33 +0300

    откорректирован файл LoggingAspect для залогирования значений параметров вызова и выходных значений методов сервисов

--- src/main/java/com/itexus/aspect/LoggingAspect.java
+++ src/main/java/com/itexus/aspect/LoggingAspect.java
@@ -1,8 +1,8 @@
 package com.itexus.aspect;
 
 import com.itexus.util.ApplicationContext;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +21,35 @@ public class LoggingAspect {
         this.messageSource = messageSource;
     }
 
-    @After("execution(* com.itexus.service..*(..))")
-    public void logAfter(JoinPoint joinPoint) {
-        Locale currentLocale = ApplicationContext.getInstance().getLocale();
+    @Around("execution(* com.itexus.service..*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Locale currentLocale = getCurrentLocale();
         logger.info(messageSource.getMessage("message.methodCalled", null, currentLocale)
                 + joinPoint.getSignature().toShortString());
+
+        // Логирование параметров
+        logMethodParameters(joinPoint);
+
+        Object result;
+        try {
+            result = joinPoint.proceed(); // Вызов фактического метода
+        } catch (Exception e) {
+            logger.error(messageSource.getMessage("message.errorMethod", null, currentLocale), joinPoint.getSignature(), e);
+            throw e; // Повторное выбрасывание исключения
+        }
+
+        // Логирование возвращаемого значения
+        logger.info(messageSource.getMessage("message.returnValue", null, currentLocale), result);
+        return result;
+    }
+
+    private void logMethodParameters(ProceedingJoinPoint joinPoint) {
+        Locale currentLocale = getCurrentLocale();
+        Object[] args = joinPoint.getArgs();
+        logger.info(messageSource.getMessage("message.parameters", null, currentLocale), args);
+    }
+
+    private Locale getCurrentLocale() {
+        return ApplicationContext.getInstance().getLocale(); // Получение текущей локали
     }
 }
