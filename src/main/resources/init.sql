419130755ea4f8794416850f57921c88a959184f
Vadzim <shylkovad@gmail.com>
2024-11-11 22:44:14 +0300

    Добавление информации о книгах в init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -6,6 +6,7 @@ CREATE TABLE authors (
                        birth_date DATE
 );
 
+
 -- Создание таблицы genres
 CREATE TABLE genres (
                        id SERIAL PRIMARY KEY,
@@ -16,6 +17,7 @@ CREATE TABLE genres (
 CREATE TABLE books (
                        id SERIAL PRIMARY KEY,
                        title VARCHAR(255) NOT NULL,
+                       description VARCHAR(5000),
                        published_date DATE
 );
 
@@ -32,3 +34,24 @@ CREATE TABLE book_genres (
                        genre_id INT REFERENCES genres(id),
                        PRIMARY KEY (book_id, genre_id)
 );
+
+
+INSERT INTO authors (name, surname, birth_date) VALUES ('Михаил', 'Булгаков', '2000-01-01');
+INSERT INTO authors (name, surname, birth_date) VALUES ('В.С.', 'Короткевич', '2002-02-02');
+
+-- Вставка жанров
+INSERT INTO genres (name) VALUES ('Роман');
+INSERT INTO genres (name) VALUES ('Стихи');
+INSERT INTO genres (name) VALUES ('Фантастика');
+
+-- Вставка книг
+INSERT INTO books (title, description, published_date) VALUES ('Мастер и маргарита', 'Загадочное и остроумное «Евангелие от Сатаны». Роман, уникальный в российской литературе ХХ столетия.', '2022-11-11');
+INSERT INTO books (title, description, published_date) VALUES ('Колосья под серпом твоим', 'В романе создана широкая панорама жизни народа в переломный для Беларуси период.', '2024-02-04');
+
+-- Вставка связей между книгами и авторами
+INSERT INTO book_authors (book_id, author_id) VALUES (1, 1); -- Книга 1 с Автором 1
+INSERT INTO book_authors (book_id, author_id) VALUES (2, 2); -- Книга 2 с Автором 2
+
+-- Вставка связей между книгами и жанрами
+INSERT INTO book_genres (book_id, genre_id) VALUES (1, 1); -- Книга 1 с Жанром 1
+INSERT INTO book_genres (book_id, genre_id) VALUES (2, 1); -- Книга 2 с Жанром 1
