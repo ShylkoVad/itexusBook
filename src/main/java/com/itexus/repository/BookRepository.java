bb547f18994697a53809cd1e1720f8080e366e33
Vadzim <shylkovad@gmail.com>
2024-11-14 22:48:07 +0300

    созданы и отредактированы классы Repository

--- src/main/java/com/itexus/repository/BookRepository.java
+++ src/main/java/com/itexus/repository/BookRepository.java
@@ -1,17 +1,63 @@
 package com.itexus.repository;
 
 import com.itexus.domain.Book;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface BookRepository {
-    List<Book> findAll();
+@Repository
+@Primary
+public class BookRepository {
+    private final JdbcTemplate jdbcTemplate;
 
-    Book findById(Long id);
+    private final RowMapper<Book> bookRowMapper = (rs, rowNum) -> {
+        Book book = new Book();
+        book.setId(rs.getLong("id"));
+        book.setTitle(rs.getString("title"));
+        book.setDescription(rs.getString("description"));
+        book.setPublishedDate(rs.getDate("published_date").toLocalDate());
+        return book;
+    };
 
-    void save(Book book);
+    public BookRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-    void update(Book book);
+    public List<Book> findAll() {
+        String sql = "SELECT * FROM books";
+        return jdbcTemplate.query(sql, bookRowMapper);
+    }
 
-    void delete(Long id);
+    public Book findById(Long id) {
+        String sql = "SELECT * FROM books WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, bookRowMapper);
+    }
+
+    public Long save(Book book) {
+        String sql = "INSERT INTO books (title, description, published_date) VALUES (?, ?, ?) RETURNING id";
+        return jdbcTemplate.queryForObject(sql, new Object[]{book.getTitle(), book.getDescription(), book.getPublishedDate()}, Long.class);
+    }
+
+    public void update(Book book) {
+        String sql = "UPDATE books SET title = ?, description = ?, published_date = ? WHERE id = ?";
+        jdbcTemplate.update(sql, book.getTitle(), book.getDescription(), book.getPublishedDate(), book.getId());
+    }
+
+    public void delete(Long id) {
+        String sql = "DELETE FROM books WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    public void addAuthorToBook(Long bookId, Long authorId) {
+        String sql = "INSERT INTO book_authors (book_id, author_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, bookId, authorId);
+    }
+
+    public void addGenreToBook(Long bookId, Long genreId) {
+        String sql = "INSERT INTO book_genres (book_id, genre_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, bookId, genreId);
+    }
 }
