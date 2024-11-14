31466e7c360ce276eb5aecd7330aff4d09e86090
Vadzim <shylkovad@gmail.com>
2024-11-14 22:46:50 +0300

    созданы и отредактированы классы Service

---
+++ src/main/java/com/itexus/service/AuthorService.java
@@ -0,0 +1,12 @@
+package com.itexus.service;
+
+import com.itexus.domain.Author;
+
+import java.util.List;
+
+public interface AuthorService {
+    Author findById(Long id);
+    List<Author> findAll();
+    void save(Author author);
+    void delete(Long id);
+}
