ff8c25be83ebc0efcc80084e168ea5dd4dcfd459
Vadzim <shylkovad@gmail.com>
2024-11-14 22:42:54 +0300

    добавил коскадное удаление в init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -23,14 +23,14 @@ CREATE TABLE books (
 
 -- Создание таблицы book_authors (Связь между книгами и авторами)
 CREATE TABLE book_authors (
-                       book_id INT REFERENCES books(id),
+                       book_id INT REFERENCES books(id) ON DELETE CASCADE,
                        author_id INT REFERENCES authors(id),
                        PRIMARY KEY (book_id, author_id)
 );
 
 -- Создание таблицы book_genres (Связь между книгами и жанрами)
 CREATE TABLE book_genres (
-                       book_id INT REFERENCES books(id),
+                       book_id INT REFERENCES books(id) ON DELETE CASCADE,
                        genre_id INT REFERENCES genres(id),
                        PRIMARY KEY (book_id, genre_id)
 );
