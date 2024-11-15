7c50347fe3213bdf23504710630529ddb80013c2
Vadzim <shylkovad@gmail.com>
2024-11-15 19:43:16 +0300

    добавил @Bean в AppConfig

--- src/main/java/com/itexus/config/AppConfig.java
+++ src/main/java/com/itexus/config/AppConfig.java
@@ -1,5 +1,14 @@
 package com.itexus.config;
 
+import com.itexus.repository.AuthorRepository;
+import com.itexus.repository.BookRepository;
+import com.itexus.repository.GenreRepository;
+import com.itexus.service.AuthorService;
+import com.itexus.service.BookService;
+import com.itexus.service.GenreService;
+import com.itexus.service.impl.AuthorServiceImpl;
+import com.itexus.service.impl.BookServiceImpl;
+import com.itexus.service.impl.GenreServiceImpl;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @ComponentScan(basePackages = "com.itexus")
@@ -19,5 +29,35 @@ public class AppConfig {
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public BookRepository bookRepository(JdbcTemplate jdbcTemplate) {
+        return new BookRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public BookService bookService(BookRepository bookRepository) {
+        return new BookServiceImpl(bookRepository);
+    }
+
+    @Bean
+    public AuthorRepository authorRepository(JdbcTemplate jdbcTemplate) {
+        return new AuthorRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public AuthorService authorService(AuthorRepository authorRepository) {
+        return new AuthorServiceImpl(authorRepository);
+    }
+
+    @Bean
+    public GenreRepository genreRepository(JdbcTemplate jdbcTemplate) {
+        return new GenreRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public GenreService genreService(GenreRepository genreRepository) {
+        return new GenreServiceImpl(genreRepository);
     }
 }
