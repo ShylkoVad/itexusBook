c9095f082cae1ec9802b054261e95f7cf2b13f00
Vadzim <shylkovad@gmail.com>
2024-12-10 18:49:15 +0300

    изменил init.sql - в связи с тем что одной книге - один жанр

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -18,7 +18,8 @@ CREATE TABLE books (
                        id SERIAL PRIMARY KEY,
                        title VARCHAR(255) NOT NULL,
                        description VARCHAR(5000),
-                       published_date DATE
+                       published_date DATE,
+                       genre_id INT REFERENCES genres(id)
 );
 
 -- Создание таблицы book_authors (Связь между книгами и авторами)
@@ -26,13 +27,6 @@ CREATE TABLE book_authors (
                        book_id INT REFERENCES books(id) ON DELETE CASCADE,
                        author_id INT REFERENCES authors(id),
                        PRIMARY KEY (book_id, author_id)
-);
-
--- Создание таблицы book_genres (Связь между книгами и жанрами)
-CREATE TABLE book_genres (
-                       book_id INT REFERENCES books(id) ON DELETE CASCADE,
-                       genre_id INT REFERENCES genres(id),
-                       PRIMARY KEY (book_id, genre_id)
 );
 
 -- Вставка авторов
@@ -45,19 +39,15 @@ INSERT INTO genres (name) VALUES ('Стихи');
 INSERT INTO genres (name) VALUES ('Фантастика');
 
 -- Вставка книг
-INSERT INTO books (title, description, published_date) VALUES ('Мастер и маргарита',
+INSERT INTO books (title, description, published_date, genre_id) VALUES ('Мастер и маргарита',
                                                                'Загадочное и остроумное «Евангелие от Сатаны». ' ||
                                                                'Роман, уникальный в российской литературе ХХ столетия.',
-                                                               '2022-11-11');
-INSERT INTO books (title, description, published_date) VALUES ('Колосья под серпом твоим',
+                                                               '2022-11-11', 1);
+INSERT INTO books (title, description, published_date, genre_id) VALUES ('Колосья под серпом твоим',
                                                                'В романе создана широкая панорама жизни народа ' ||
                                                                'в переломный для Беларуси период.',
-                                                               '2024-02-04');
+                                                               '2024-02-04', 1);
 
 -- Вставка связей между книгами и авторами
 INSERT INTO book_authors (book_id, author_id) VALUES (1, 1); -- Книга 1 с Автором 1
 INSERT INTO book_authors (book_id, author_id) VALUES (2, 2); -- Книга 2 с Автором 2
-
--- Вставка связей между книгами и жанрами
-INSERT INTO book_genres (book_id, genre_id) VALUES (1, 1); -- Книга 1 с Жанром 1
-INSERT INTO book_genres (book_id, genre_id) VALUES (2, 1); -- Книга 2 с Жанром 1
