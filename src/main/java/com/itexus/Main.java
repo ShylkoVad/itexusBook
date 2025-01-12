96bba995bff9bc9cb3cbc391dfe078437f618a5a
Vadzim <shylkovad@gmail.com>
2025-01-12 15:07:15 +0300

    класс Main для Spring Boot

--- src/main/java/com/itexus/Main.java
+++ src/main/java/com/itexus/Main.java
@@ -1,13 +1,11 @@
 package com.itexus;
 
-import com.itexus.controller.BookController;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(com.itexus.config.AppConfig.class);
-        BookController bookController = context.getBean(BookController.class);
-        bookController.run();
+        SpringApplication.run(Main.class, args);
     }
 }
