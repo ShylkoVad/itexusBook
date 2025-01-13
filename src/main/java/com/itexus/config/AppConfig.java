361dfffc01d6dd4ab6cfa7eb80b4367c9f751003
Vadzim <shylkovad@gmail.com>
2025-01-13 20:56:47 +0300

    изменены классы AppConfig под spring boot

--- src/main/java/com/itexus/config/AppConfig.java
+++ src/main/java/com/itexus/config/AppConfig.java
@@ -1,27 +1,15 @@
 package com.itexus.config;
 
-import com.itexus.repository.AuthorRepository;
-import com.itexus.repository.BookRepository;
-import com.itexus.repository.GenreRepository;
-import com.itexus.service.AuthorService;
-import com.itexus.service.BookService;
-import com.itexus.service.GenreService;
-import com.itexus.service.impl.AuthorServiceImpl;
-import com.itexus.service.impl.BookServiceImpl;
-import com.itexus.service.impl.GenreServiceImpl;
-import org.hibernate.SessionFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.itexus")
 @EnableAspectJAutoProxy
-@Import(HibernateConfig.class) // Импорт класса HibernateConfig
 public class AppConfig {
 
     @Bean
@@ -29,36 +17,7 @@ public class AppConfig {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3600); // Кэшировать на 1 час
         return messageSource;
-    }
-
-    @Bean
-    public BookRepository bookRepository(SessionFactory sessionFactory) {
-        return new BookRepository(sessionFactory);
-    }
-
-    @Bean
-    public BookService bookService(BookRepository bookRepository) {
-        return new BookServiceImpl(bookRepository);
-    }
-
-    @Bean
-    public AuthorRepository authorRepository(SessionFactory sessionFactory) {
-        return new AuthorRepository(sessionFactory);
-    }
-
-    @Bean
-    public AuthorService authorService(AuthorRepository authorRepository) {
-        return new AuthorServiceImpl(authorRepository);
-    }
-
-    @Bean
-    public GenreRepository genreRepository(SessionFactory sessionFactory) {
-        return new GenreRepository(sessionFactory);
-    }
-
-    @Bean
-    public GenreService genreService(GenreRepository genreRepository) {
-        return new GenreServiceImpl(genreRepository);
     }
 }
