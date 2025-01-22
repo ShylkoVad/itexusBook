0fd3a97946f8bc6a02215856806983f12805889d
Vadzim <shylkovad@gmail.com>
2025-01-22 19:46:39 +0300

    откоректированны классы для Author

--- src/main/java/com/itexus/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/itexus/service/impl/AuthorServiceImpl.java
@@ -1,47 +1,66 @@
 package com.itexus.service.impl;
 
 import com.itexus.domain.Author;
+import com.itexus.dto.AuthorDTO;
+import com.itexus.dto.converters.AuthorConverters;
 import com.itexus.repository.AuthorRepository;
 import com.itexus.service.AuthorService;
-import org.springframework.context.annotation.Primary;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @Service
-@Primary
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
+    private final AuthorConverters authorConverters;
 
-    public AuthorServiceImpl(AuthorRepository authorRepository) {
+    public AuthorServiceImpl(AuthorRepository authorRepository, AuthorConverters authorConverters) {
         this.authorRepository = authorRepository;
+        this.authorConverters = authorConverters;
     }
 
     @Override
-    public Author findById(Long id) {
-        return authorRepository.findById(id);
+    public AuthorDTO findByIdAuthor(Long id) {
+        return authorConverters.toDTO(authorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Автора с id %d не найдено.", id))));
     }
 
     @Override
-    public List<Author> findAll() {
-        return authorRepository.findAll();
+    public List<AuthorDTO> findAllAuthors() {
+        return authorRepository.findAll().stream().map(authorConverters::toDTO).toList();
     }
 
     @Override
-    public void save(Author author) {
-        authorRepository.save(author);
+    public AuthorDTO saveAuthor(AuthorDTO authorDTO) {
+        Author author = authorConverters.fromDTO(authorDTO);
+        author = authorRepository.save(author);
+        return authorConverters.toDTO(author);
     }
 
     @Override
-    public void delete(Long id) {
-        authorRepository.delete(id);
+    public void deleteAuthor(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Автора с id %d не найдено.", id)));
+        authorRepository.delete(author);
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public List<Author> findAuthorsByBookId(Long bookId) {
-        return authorRepository.findAuthorsByBookId(bookId);
+    public AuthorDTO updateAuthor(AuthorDTO authorDTO) {
+        Author author = authorRepository.findById(authorDTO.getId())
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Автора с id %d не найдено.", authorDTO.getId())));
+        author.setName(authorDTO.getName());
+        author.setSurname(authorDTO.getSurname());
+        author.setBirthDate(authorDTO.getBirthDate());
+        return authorConverters.toDTO(authorRepository.save(author));
+    }
+
+    @Override
+    public List<AuthorDTO> findAuthorsByBookId(Long bookId) {
+
+        return null;
     }
 }
