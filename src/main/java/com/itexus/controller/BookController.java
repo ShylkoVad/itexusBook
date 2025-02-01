b38fb95380a95c943f74087c3548c1d641955d08
Vadzim <shylkovad@gmail.com>
2025-02-01 19:16:45 +0300

    внес изменения в контроллер для загрузки и выгрузки файла

--- src/main/java/com/itexus/controller/BookController.java
+++ src/main/java/com/itexus/controller/BookController.java
@@ -3,7 +3,9 @@ package com.itexus.controller;
 import com.itexus.dto.BookDTO;
 import com.itexus.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -51,5 +56,29 @@ public class BookController {
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
+    }
+
+    // Метод для загрузки изображения
+    @PostMapping("/{id}/upload-image")
+    public ResponseEntity<String> uploadImage(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        try {
+            String imageId = bookService.uploadImage(id, file); // Метод в сервисе для обработки загрузки
+            return ResponseEntity.ok(imageId);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка загрузки изображения");
+        }
+    }
+
+    // Метод для выгрузки изображения
+    @GetMapping("/{id}/image")
+    public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
+        byte[] imageData = bookService.getImage(id); // Метод в сервисе для получения изображения
+        if (imageData != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG); // Указать тип изображения
+            return new ResponseEntity<>(imageData, headers, HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
