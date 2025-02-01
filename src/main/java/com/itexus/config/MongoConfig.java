24bb907e557fdad07b1a6d32d247b23c963d9f5c
Vadzim <shylkovad@gmail.com>
2025-02-01 12:38:37 +0300

    создал MongoConfig

---
+++ src/main/java/com/itexus/config/MongoConfig.java
@@ -0,0 +1,23 @@
+package com.itexus.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+    private final MongoClient mongoClient;
+
+    public MongoConfig(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+
+    @Bean
+    public GridFSBucket gridFSBucket() {
+        MongoDatabase database = mongoClient.getDatabase("Books_library");
+        return GridFSBuckets.create(database);
+    }
+}
