8d2ff9700c06059fc24ed59dab20dbe31ec537e4
Vadzim <shylkovad@gmail.com>
2025-02-16 15:06:24 +0300

    добавил что столбец email не может быть пустым

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -37,7 +37,7 @@ CREATE TABLE users (
                        id SERIAL PRIMARY KEY,
                        username VARCHAR(50) NOT NULL,
                        password VARCHAR(255) NOT NULL,
-                       email VARCHAR(100)
+                       email VARCHAR(100) NOT NULL
 );
 
 -- Создание таблицы roles
