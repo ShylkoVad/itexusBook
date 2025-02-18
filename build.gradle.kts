1047c2c949a387e8858d3366aead194f8559a673
Vadzim <shylkovad@gmail.com>
2025-02-18 17:53:02 +0300

    добавил зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,15 +21,24 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.postgresql:postgresql:42.7.4")  // для добавления зависимости PostgreSQL JDBC драйвера
-    implementation("org.hibernate:hibernate-core:7.0.0.Beta3")  // Hibernate Core
+//    implementation("org.hibernate:hibernate-core:7.0.0.Beta3")  // Hibernate Core
 
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.4.2")
 
     implementation("org.springframework.boot:spring-boot-starter-logging")  // Spring Boot Starter для логирования
-    implementation("org.slf4j:slf4j-api:2.1.0-alpha1") // SLF4J API
+//    implementation("org.slf4j:slf4j-api:2.1.0-alpha1") // SLF4J API
 
     implementation("org.springframework.boot:spring-boot-starter-security:3.4.2")
     implementation("org.springframework.security:spring-security-test:6.4.2")
+    implementation("org.springframework.security:spring-security-config:6.4.2")
+    implementation("org.springframework.security:spring-security-web:6.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.4.2")
+    implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+
+    implementation("io.jsonwebtoken:jjwt:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<JavaCompile> {
