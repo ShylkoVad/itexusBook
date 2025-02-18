c98f7bd8637beb7b0551c25860bd4340322145fe
Vadzim <shylkovad@gmail.com>
2025-02-18 17:59:55 +0300

    создал репозитории

---
+++ src/main/java/com/itexus/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.itexus.repository;
+
+import com.itexus.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
---
+++ src/main/java/com/itexus/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.itexus.repository;
+
+import com.itexus.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmailAndPassword(String email, String password);
+
+    Optional<User> findByEmail(String email);
+}
