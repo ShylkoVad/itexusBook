54088a3adb0f5afed97b6b5fd6a2b70f20794f90
Vadzim <shylkovad@gmail.com>
2025-03-29 20:37:17 +0300

    обновленный BookRepository - поиск Id автора по Id книги в таблице book_authors

--- book-service/src/main/java/com/itexus/book/repository/BookRepository.java
+++ book-service/src/main/java/com/itexus/book/repository/BookRepository.java
@@ -2,8 +2,16 @@ package com.itexus.book.repository;
 
 import com.itexus.book.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query(value = "SELECT author_id FROM book_authors WHERE book_id = :bookId", nativeQuery = true)
+    List<Long> findAuthorIdsByBookId(@Param("bookId") Long bookId);
 }
+
