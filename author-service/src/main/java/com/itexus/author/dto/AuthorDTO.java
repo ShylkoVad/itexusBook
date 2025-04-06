b2013d0028e149b31dffc193ca1cfe1b1f6afd31
Vadzim <shylkovad@gmail.com>
2025-04-06 17:56:13 +0300

    добавил чтобы строки с null не выводились

--- author-service/src/main/java/com/itexus/author/dto/AuthorDTO.java
+++ author-service/src/main/java/com/itexus/author/dto/AuthorDTO.java
@@ -1,5 +1,6 @@
 package com.itexus.author.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // Исключаем поля с null
 public class AuthorDTO {
 
     private Long id;
--- genre-service/src/main/java/com/itexus/genre/dto/GenreDTO.java
+++ genre-service/src/main/java/com/itexus/genre/dto/GenreDTO.java
@@ -1,5 +1,6 @@
 package com.itexus.genre.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // Исключаем поля с null
 public class GenreDTO {
 
     private Long id;
